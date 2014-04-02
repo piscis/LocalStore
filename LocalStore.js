e89ab091a39e1cf5dcbc47932109de5455a02396
Ryan Blackman <rblackman24@gmail.com>
2014-04-02 11:13:35 -0500

    changed function definitions to use prototypes

--- LocalStore.js
+++ LocalStore.js
@@ -5,74 +5,6 @@ var LocalStore = function () {
 	/// Interact with localStorage
 	/// </summary>
 
-	function clear(key) {
-		/// <signature>
-		/// <summary>
-		/// Clear a value with a specific key.
-		/// </summary>
-		/// <param name="key">The local storage key to clear.</param>
-		/// </signature>
-		/// <signature>
-		/// <summary>
-		/// Clear all local storage.
-		/// </summary>
-		/// </signature>
-		if (key) {
-			localStorage.removeItem(key);
-		} else {
-			localStorage.clear();
-		}
-	}
-	
-	function hasKey(key) {
-		/// <summary>
-		/// See if local storage has a specific key.
-		/// </summary>
-		/// <param name="key">The key to lookup.</param>
-		/// <returns>true if the key exists in local storage, false if not.</returns>
-		if (!key) {
-			return false;
-		}
-		return key in localStorage;
-	}
-
-	function load(key) {
-		/// <summary>
-		/// Load an object from local storage.
-		/// </summary>
-		/// <param name="key">The key to lookup.</param>
-		/// <returns>The value stored in local storage.  As JSON if it is not a string.</returns>
-		if (key) { // make sure we have been given a key to load
-			// the value stored in local storage
-			var value = localStorage[key];
-
-			try {
-				return window.JSON.parse(value);
-			} catch (ex) {
-				return value;
-			}
-		} else {
-			throw 'you must provide a key';
-		}
-	}
-
-	function save(key, value) {
-		/// <summary>
-		/// Save an object to localStorage
-		/// </summary>
-		/// <param name="key">The key to store the value with.</param>
-		/// <param name="value">The object or string to store.</param>
-		if (key && value) {
-			try {
-				localStorage[key] = window.JSON.stringify(value);
-			} catch (ex) {
-				localStorage[key] = value;
-			}
-		} else {
-			throw 'you must provide both a key and a value';
-		}
-	}
-
 	// Feature detection
 	(function () {
 		var hasStorage = (function () {
@@ -98,37 +30,72 @@ var LocalStore = function () {
 		}
 	})();
 
-	return {
-		/// <signature>
-		/// <summary>
-		/// Clear a value with a specific key.
-		/// </summary>
-		/// <param name="key">The local storage key to clear.</param>
-		/// </signature>
-		/// <signature>
-		/// <summary>
-		/// Clear all local storage.
-		/// </summary>
-		/// </signature>
-		clear: clear,
-		/// <summary>
-		/// Load an object from local storage.
-		/// </summary>
-		/// <param name="key">The key to lookup.</param>
-		/// <returns>The value stored in local storage.  As JSON if it is not a string.</returns>
-		load: load,
-		/// <summary>
-		/// See if local storage has a specific key.
-		/// </summary>
-		/// <param name="key">The key to lookup.</param>
-		/// <returns>true if the key exists in local storage, false if not.</returns>
-		hasKey: hasKey,
-		/// <summary>
-		/// Save an object to localStorage
-		/// </summary>
-		/// <param name="key">The key to store the value with.</param>
-		/// <param name="value">The object or string to store.</param>
-		save: save
-	};
-
 };
+
+LocalStore.prototype.clear = function (key) {
+	/// <signature>
+	/// <summary>
+	/// Clear a value with a specific key.
+	/// </summary>
+	/// <param name="key">The local storage key to clear.</param>
+	/// </signature>
+	/// <signature>
+	/// <summary>
+	/// Clear all local storage.
+	/// </summary>
+	/// </signature>
+	if (key) {
+		localStorage.removeItem(key);
+	} else {
+		localStorage.clear();
+	}
+}
+
+LocalStore.prototype.hasKey = function (key) {
+	/// <summary>
+	/// See if local storage has a specific key.
+	/// </summary>
+	/// <param name="key">The key to lookup.</param>
+	/// <returns>true if the key exists in local storage, false if not.</returns>
+	if (!key) {
+		return false;
+	}
+	return key in localStorage;
+}
+
+LocalStore.prototype.load = function (key) {
+	/// <summary>
+	/// Load an object from local storage.
+	/// </summary>
+	/// <param name="key">The key to lookup.</param>
+	/// <returns>The value stored in local storage.  As JSON if it is not a string.</returns>
+	if (key) { // make sure we have been given a key to load
+		// the value stored in local storage
+		var value = localStorage[key];
+
+		try {
+			return window.JSON.parse(value);
+		} catch (ex) {
+			return value;
+		}
+	} else {
+		throw 'you must provide a key';
+	}
+}
+
+LocalStore.prototype.save = function (key, value) {
+	/// <summary>
+	/// Save an object to localStorage
+	/// </summary>
+	/// <param name="key">The key to store the value with.</param>
+	/// <param name="value">The object or string to store.</param>
+	if (key && value) {
+		try {
+			localStorage[key] = window.JSON.stringify(value);
+		} catch (ex) {
+			localStorage[key] = value;
+		}
+	} else {
+		throw 'you must provide both a key and a value';
+	}
+}
