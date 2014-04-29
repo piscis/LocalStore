9764e7352c0997e21c66afeb7078ae6a1c9b8da5
Ryan Blackman <rblackman24@gmail.com>
2014-04-29 10:41:25 -0500

    added minification and watching

--- gruntfile.js
+++ gruntfile.js
@@ -1,6 +1,5 @@
 module.exports = function(grunt) {
 
-    // 1. All configuration goes here
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
 
@@ -14,18 +13,35 @@ module.exports = function(grunt) {
                     sourceMap: true,
                     declaration: true,
                     noImplicitAny: true,
-                    useTabIndent: true,
-                    watch: 'ts'
+                    useTabIndent: true
+                }
+            }
+        },
+        uglify: {
+            build: {
+                src: 'js/LocalStore.js',
+                dest: 'js/LocalStore.min.js'
+            },
+            options: {
+                sourceMap: true
+            }
+        },
+        watch: {
+            scripts: {
+                files: ['ts/*.ts'],
+                tasks: ['typescript', 'uglify'],
+                options: {
+                    spawn: false
                 }
             }
         }
 
     });
 
-    // 3. Where we tell Grunt we plan to use this plug-in.
     grunt.loadNpmTasks('grunt-typescript');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-watch');
 
-    // 4. Where we tell Grunt what to do when we type "grunt" into the terminal.
-    grunt.registerTask('default', ['typescript']);
+    grunt.registerTask('default', ['typescript', 'uglify', 'watch']);
 
 };
